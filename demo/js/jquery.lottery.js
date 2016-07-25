1fa254c43f83adf73bb11bcea0191478cefe3512
limeng <limeng@aixuedai.com>
2016-07-25 20:06:14 +0800

    updated

--- demo/js/jquery.lottery.js
+++ demo/js/jquery.lottery.js
@@ -20,8 +20,11 @@
 */
 $.fn.lottery = function(options) {
     var me = this,
+        mleft = me.offset().left,
+        mtop = me.offset().top,
         backTemplate = '<div class="fQ_front">{{back}}</div>',
         frontTemplate = '<div class="fQ_back">{{front}}</div>',
+
         //重新洗牌函数
         _mess = function(arr) {
             var _floor = Math.floor, _random = Math.random, 
@@ -46,6 +49,64 @@ $.fn.lottery = function(options) {
                 result = result.replace('{{' + key + '}}', obj[key]);
             }
             return result;
+        },
+        _endAnimation = function(tarray, parray, i) {
+            if (i < tarray.length) {
+                var t = $(tarray[i]),
+                    thisLeft = t.offset().left - mleft,
+                    thisTop = t.offset().top - mtop,
+                    start = 0,
+                    stepLeft, stepTop, intervalTimer;
+                stepLeft = (parray[i].left - thisLeft) / 50;
+                stepTop = (parray[i].top - thisTop) / 50;
+                intervalTimer = setInterval(function(){
+                    if (start === 50) {
+                        clearInterval(intervalTimer);
+                    } else if (start === 5) {
+                        _endAnimation(tarray, parray, i + 1);
+                    } else if (start === 49) {
+                        t.css({
+                            left: parray[i].left,
+                            top: parray[i].top
+                        });
+                    } else {
+                        t.css({
+                            left: t.offset().left - mleft + stepLeft,
+                            top: t.offset().top - mtop + stepTop
+                        });
+                    }
+                    start++;
+                }, 20);
+            }
+        },
+        _startAnimation = function(tarray, i, prop) {
+            if (i < tarray.length) {
+                var t = $(tarray[i]),
+                    thisLeft = t.offset().left - mleft,
+                    thisTop = t.offset().top - mtop,
+                    start = 0,
+                    stepLeft, stepTop, intervalTimer;
+                stepLeft = (prop.left - thisLeft) / 50;
+                stepTop = (prop.top - thisTop) / 50;
+                intervalTimer = setInterval(function(){
+                    if (start === 50) {
+                        clearInterval(intervalTimer);
+                    } else if (start === 5) {
+                        _startAnimation(tarray, i + 1, prop);
+                    } else if (start === 49) {
+                        t.css({
+                            left: prop.left,
+                            top: prop.top
+                        });
+                    } else {
+                        t.css({
+                            left: t.offset().left - mleft + stepLeft,
+                            top: t.offset().top - mtop + stepTop
+                        });
+                    }
+                    start++;
+                }, 20);
+            }
         };
     if (!options) {
         console.error('param error: "options" is required.');
@@ -80,30 +141,41 @@ $.fn.lottery = function(options) {
                 //点击了开始翻牌按钮
                 var lotteryItems = me.find('.fQ_container'), 
                     targetItem = me.find('.start'),
-                    positions = [];
+                    meLeft = me.offset().left,
+                    meTop = me.offset().top,
+                    positions = [],
+                    lotteryCopy = [];
                 if (!$(this).data('finished')){
                     $(this).data('finished', 'done');
                     lotteryItems.data('start', 'true');
                     $('.fQ_container').addClass('fQ_flip');
-                    lotteryItems.animate(targetItem.position(), 1000);
+
+                    setTimeout(function(){
+                        _startAnimation(lotteryItems, 0, 
+                            {
+                                left: targetItem.offset().left - meLeft,
+                                top: targetItem.offset().top - meTop
+                            });
+                    }, 800);
 
                     //随机返还到原来的位置
                     lotteryItems.each(function(){
-                        positions.push($(this).position());
+                        // positions.push($(this).offset());
+                        positions.push({
+                            left: $(this).offset().left - meLeft,
+                            top: $(this).offset().top - meTop,
+                        });
+                        lotteryCopy.push($(this));
                     });
 
                     //将数组位置重新洗牌
                     _mess(positions);
-                    _mess(lotteryItems);
+                    _mess(lotteryCopy);
 
-                    function lotteryAnimate(i) {
-                        if (i < lotteryItems.length) {
-                            $(lotteryItems[i]).animate(positions[i], 200, function(){
-                                lotteryAnimate(i + 1);
-                            });
-                        }
-                    }
-                    lotteryAnimate(0);
+                    setTimeout(function(){
+                        _endAnimation(lotteryCopy, positions, 0);
+                    },3000);
+                    
                     if (options.startLottery && options.startLottery instanceof Function) {
                         options.startLottery();
                     }
